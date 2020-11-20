8b1b4eb6ef2e43f56748a2ecef17c50be7fcc61b
Ishaat Chowdhury <ishaat@ualberta.ca>
2020-11-19 22:57:50 -0800

    Create opensuse-tw-jeos-setup.sh

---
+++ opensuse-tw-jeos-setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# args
+hostname=${hostname:-jeos-hyperv}
+username=${username:-ishaat}
+
+# parse args
+# taken from: https://brianchildress.co/named-parameters-in-bash/
+while [ $# -gt 0 ]; do
+    if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+        # echo $1 $2 // Optional to see the parameter:value result
+    fi
+    shift
+done
+
+echo "Changing hostname..."
+hostnamectl set-hostname $hostname
+
+echo "Installing base packages..."
+zypper in -y -t pattern enhanced_base console sw_management documentation
+zypper in -y command-not-found sudo
+
+echo "Adding groups..."
+groupadd -f users
+groupadd -f wheel
+groupadd -f docker
+
+echo "Create user..."
+useradd -g users -G wheel,docker -m $username
+passwd $username
+
+echo "Logging in as user..."
+su - $username
+
